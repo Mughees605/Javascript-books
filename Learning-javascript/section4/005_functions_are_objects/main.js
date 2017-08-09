dae610fd879df8783ec62b70e78a5453a98e5f59
mughees605 <mughees605@gmail.com>
2017-08-09 17:29:52 +0500

    functions are objects

---
+++ Learning-javascript/section4/005_functions_are_objects/main.js
@@ -0,0 +1,10 @@
+/**
+ * - In javascript functions are objects.
+ * - FIRST CLASS FUNCTIONS: Everything you can do with other types you can do with functions.
+ */
+function greet(){
+    console.log("hi")
+}
+
+// I added a property to function
+greet.language = "english";

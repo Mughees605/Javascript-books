1cd9e7383c7402bf841d25578bdbe33c2541cef7
mughees605 <mughees605@gmail.com>
2017-08-10 13:39:49 +0500

    minor fix

--- Learning-javascript/section4/005_functions_are_objects/main.js
+++ Learning-javascript/section4/005_functions_are_objects/main.js
@@ -1,6 +1,10 @@
 /**
  * - In javascript functions are objects.
- * - FIRST CLASS FUNCTIONS: Everything you can do with other types you can do with functions.
+ * - FIRST CLASS FUNCTIONS:  * 	
+ * - everything you can do with other types such as objects, strings, numbers, and booleans you can do with functions
+ * 	- assign variables to have a value thats a function
+ *  - pass functions around as parameters to other functions
+ *  - create functions on the fly with a kind of literal syntax
  */
 function greet(){
     console.log("hi")
@@ -8,3 +12,5 @@ function greet(){
 
 // I added a property to function
 greet.language = "english";
+
+greet()
